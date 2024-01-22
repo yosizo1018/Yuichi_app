6f19197f53d3553416b727e49097ad18e1ffdfa4
yosizo1018 <uoly1018@gmail.com>
2024-01-22 23:01:22 +0900

    finish

--- pages/過去の会社実績.py
+++ pages/過去の会社実績.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 #データ分析関連
 df = pd.read_csv('./data/過去実績.csv', index_col='月')
@@ -12,8 +11,3 @@ st.text('【過去の月ごとの生ビール飲酒実績】')
 st.line_chart(dff)
 st.text('【2023年の飲み会参加人数実績】')
 st.bar_chart(df['2023年(人)'])
-# #matplotlibを使用したグラフ --うまくできない--
-# fig, ax = plt.subplots()
-# ax.plot(dff.index, dff['2023年(杯)'])
-# ax.set_title('mat_dayo')
-# st.pyplot(fig)
